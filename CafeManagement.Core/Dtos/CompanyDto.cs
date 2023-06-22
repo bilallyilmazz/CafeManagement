6ff4e4e36ff53fa2a23a750dde32e1f397e81d74
bilallyilmazz <bilallyilmazz@gmail.com>
2023-06-23 00:09:08 +0300

    setting eklendi

---
+++ CafeManagement.Core/Dtos/CompanyDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManagement.Core.Dtos
+{
+    public class CompanyDto
+    {
+        public string Name { get; set; }
+
+    }
+}
